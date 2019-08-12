67d32f0934ca2b536f0f1f1f286f4010dc03d2b7
Maciej Eder <maciejeder@gmail.com>
2019-08-12 23:20:25 +0200

    several improvements in text.size.penalize()

--- R/text.size.penalize.R
+++ R/text.size.penalize.R
@@ -11,6 +11,7 @@ text.size.penalize = function(training.frequencies = NULL,
                               sample.with.replacement = FALSE,
                               iterations = 100,
                               classification.method = "delta",
+                              list.cutoff = 1000,
                               ...) {
     #
     
@@ -18,13 +19,31 @@ text.size.penalize = function(training.frequencies = NULL,
     
     
     
-##### temporary!! ######    
+    
+    ###############################################################
+    
+    # testing if multicore environment ('doMC', 'foreach') can be used
+    test_doMC = tryCatch(doMC::registerDoMC(cores = detectCores()), error = function(e) NULL)
+    test_foreach = tryCatch(foreach::foreach(i = 1:2, .combine = "rbind") %do% sum(c(1,2,3)), error = function(e) NULL)
+    
+    # switching to either parallel, or serial mode
+    if(length(test_foreach[1]) > 0 & length(test_doMC) > 0) {
+        parallel_mode = TRUE
+        doMC::registerDoMC(cores = detectCores())
+    } else {
+        parallel_mode = FALSE
+    }
+    
+    ###############################################################
+    
+    
+    
+    
+##### temporary!! ######
+#source("/Users/m/Desktop/stylo_R_package/stylo/R/performance.measures.R")
 corpus.language = "English.all"
-list.cutoff = 1000
-training.corpus = c("ABronte_Agnes", "ABronte_Tenant")
-library(doMC)
-library(foreach)
-registerDoMC(cores = cpu.cores)
+training.corpus = c("ABronte_Agnes")#, 
+#training.corpus = c("ABronte_Tenant")
 ##### temporary!! ######    
 
 # if(training.frequencies == NULL)
@@ -41,32 +60,20 @@ registerDoMC(cores = cpu.cores)
 # else:     
     test.texts = training.corpus ## or names(training.corpus) !!!!!
     #
-    
-    # starting empty variables, to collect final results
-    joint.attribution.scores = list()
-    joint.confusion.matrices = list()
-    iteration.counter = 0
-    
-    
-    # an outer loop, to iterate over text samples
-    for(test.text in test.texts) {
-        
-        # showing the currently processed sample on screen
-        message(test.text)
-        # setting a counter of iterations (= processed samples)
-        iteration.counter = iteration.counter +1
 
-    train.table = doc.term.matrix[rownames(doc.term.matrix) != test.text,]   
 
-    
-##### MAKE SURE THE TEXT EXISTS in the input.texts object!
-##### if loaded from an external object, it has to be taken into account!
-    get.test.text = input.texts[[grep(test.text, names(input.texts))]]
-    
+    message("")
+    message("Testing ", length(sample.size.coverage), " sample sizes raging from ", 
+            min(sample.size.coverage), " to ", max(sample.size.coverage), 
+            " words (or other elements),\n", 
+            "as defined by the argument 'sample.size.coverage'; for every single sample size ", 
+            iterations, " random samples are drawn, as defined by the argument 'iterations'.")
+
+
+
     # function (iterator) to get random samples from a given input text 
     get.vector.of.freqs = function(tokenized.text) {
         current.sample = sample(tokenized.text, size = current.sample.size, replace = sample.with.replacement)
-        #current.sample = make.samples(tokenized.text, sampling = "random.sampling", sample.size = current.sample.size)[[1]]
         relative.frequencies = table(current.sample) / length(current.sample) * 100
         vector.of.freqs = relative.frequencies[wordlist]
         names(vector.of.freqs) = wordlist
@@ -74,78 +81,183 @@ registerDoMC(cores = cpu.cores)
         return(vector.of.freqs)
     }
     
+    
+    
     # function (iterator) to perform the classification stage
-    perform.classification = function(mfw) {
+    perform.classification = function(no.of.features) {
         if(classification.method == "delta") {
-            bbb = perform.delta(train.table[,1:mfw], test.table[,1:mfw], z.scores.both.sets = FALSE, ...)
+            predicted_classes = perform.delta(train.table[,1:no.of.features], test.table[,1:no.of.features], z.scores.both.sets = FALSE, ...)
         }
         if(classification.method == "svm") {
-            bbb = perform.svm(train.table[,1:mfw], test.table[,1:mfw], ...)
+            predicted_classes = perform.svm(train.table[,1:no.of.features], test.table[,1:no.of.features], ...)
         }
         if(classification.method == "nsc") {
-            bbb = perform.nsc(train.table[,1:mfw], test.table[,1:mfw], ...)
+            predicted_classes = perform.nsc(train.table[,1:no.of.features], test.table[,1:no.of.features], ...)
         }
 #### this class matching seems a bit weird!
-        results = sum(as.numeric(bbb == gsub("_.*" ,"", test.text)))
-        attr(results, "confusion_matrix") = attr(bbb, "confusion_matrix")
+        actual_classes = gsub("_.*" ,"", names(predicted_classes))
+        predicted_classes = as.character(predicted_classes)
+        training_classes = gsub("_.*" ,"", rownames(train.table))
+        
+        predicted = factor(predicted_classes, levels = unique(as.character(training_classes)))
+        expected  = as.factor(actual_classes)
+        confusion_matrix = as.matrix(table(expected, predicted))
+        
+        results = confusion_matrix
+        accuracy = sum(actual_classes == predicted_classes)
+        attr(results, "accuracy") = accuracy
         return(results)
     }
     
     
-    # starting a new (empty) variable
-    compact.results = c()
-    full.results = list()
-    counter.alt = 0
+    # function to compute Simpson's index of diversity
+    get.dispersion = function(x) {
+        # l = sum( x ^ 2 )
+        #  - sum( x * log(x))
+        # y = sum(x * (x-1)) / sum(x) * (sum(x) -1) 
+        l = sum(x * (x-1)) / (sum(x) * (sum(x) -1))
+#l = ( 4 * sum(x) * (sum(x)-1) * (sum(x)-2) * sum((x/sum(x))^3) + 2 * sum(x) * (sum(x)-1) * sum((x/sum(x))^2) - 2 * sum(x) * (sum(x-1)) * (2*sum(x)-3) * (sum((x/sum(x))^2)^2) ) / ( (sum(x) * (sum(x)-1))^2 )
+
+        return(l)
+    }
     
     
-    # now, iterating over specified range of sample sizes to asses
-    for(current.sample.size in sample.size.coverage) {
-        
-        message(".", appendLF = FALSE)
-        
-        counter.alt = counter.alt + 1
+    # variance
+    # Ds = ( sum( (x / sum(x) )^3 ) - (sum( (x / sum(x) )^2 )^2 ) ) / (sum(x)/4)
+    
+    # a better version:
+    # Ds = ( 4 * sum(x) * (sum(x)-1) * (sum(x)-2) * sum((x/sum(x))^3) + 2 * sum(x) * (sum(x)-1) * sum((x/sum(x))^2) - 2 * sum(x) * (sum(x-1)) * (2*sum(x)-3) * (sum((x/sum(x))^2)^2) ) / ( (sum(x) * (sum(x)-1))^2 )
+    
+    
+    
 
-##### ADJUST FOR #iterations (default = 100)
+    
+    # starting empty variables, to collect final results
+    joint.accuracy.scores = list()
+    joint.diversity.scores = list()
+    joint.confusion.matrices = list()
+    iteration.counter = 0    
+    
+    
+    # an outer loop, to iterate over text samples
+    for(test.text in test.texts) {
         
-        # a loop involving many cores, to extract text samples in 100 iterations 
-        test.table = foreach(i = 1:100, .combine = "rbind") %dopar% get.vector.of.freqs(get.test.text)
-        rownames(test.table) = paste(test.text, 1:100, sep="_")    
+        # showing the currently processed sample on screen
+        message("")
+        message(test.text)
+        # setting a counter of iterations (= processed samples)
+        iteration.counter = iteration.counter +1
 
-        # another loop (the main one!), aka classification
-        # which involves different vectors of features
-        classify = foreach(f = mfw) %dopar% perform.classification(f)
-        # collecting attr(ddd, "confusion_matrix") in the form of list
-        confusion.matrix = list()
-        for(g in 1:length(classify)) {
-            confusion.matrix[[g]] = attr(classify[[g]], "confusion_matrix")
+        train.table = doc.term.matrix[rownames(doc.term.matrix) != test.text,]   
+
+    
+##### MAKE SURE THE TEXT EXISTS in the input.texts object!
+##### if loaded from an external object, it has to be taken into account!
+        get.test.text = input.texts[[grep(test.text, names(input.texts))]]
+        
+        
+        
+        
+        # starting a new (empty) variables
+        accuracy_all = c()
+        diversity_all = c()
+        confusion_matrices_all = list()
+        counter.alt = 0
+
+        
+        # now, iterating over specified range of sample sizes to asses
+        for(current.sample.size in sample.size.coverage) {
+            
+            counter.alt = counter.alt + 1
+            
+            # a short message on screen
+            message(".", appendLF = FALSE)
+            if(counter.alt %% 64 == 0) {
+                message("\n", appendLF = FALSE)
+            }
+            
+            # sampling N times from the original text
+            if(parallel_mode == TRUE) {
+                # a loop involving many cores, to extract text samples in N iterations 
+                test.table = foreach::foreach(i = 1:iterations, .combine = "rbind") %dopar% get.vector.of.freqs(get.test.text)
+            } else {
+                # a loop using one CPU core: a classic solution
+                test.table = c()
+                for(i in 1:iterations) {
+                    g = get.vector.of.freqs(get.test.text)
+                    test.table = rbind(test.table, g)
+                }
+            }
+            
+            rownames(test.table) = paste(test.text, 1:iterations, sep="_")    
+            
+            # another loop (the main one!), aka classification
+            # which involves different vectors of features
+            if(parallel_mode == TRUE) {
+                # this version involves many CPU cores
+                classify_results = foreach::foreach(f = mfw) %dopar% perform.classification(f)
+            } else {
+                # ...and this is a one-core equivalent of the above
+                classify_results = list()
+                no_of_f = 0
+                for(f in mfw) {
+                    no_of_f = no_of_f + 1
+                    b = perform.classification(f)
+                    classify_results[[no_of_f]] = b
+                }
+            }
+            
+            predicted_classes = colnames(classify_results[[1]])
+            # scaling the accuracy values according to the number of iterations
+            # so that the final results fall into the range {0,1}.
+#            classify_results = lapply(classify_results, function(x) (x / iterations) )
+            
+            # retrieving accuracies from the results (stored as an attribute)
+            accuracy = sapply(classify_results, function(x) attr(x, "accuracy"))
+            # scaling the compact accuracy values as well (see above)
+            accuracy = accuracy / iterations
+            
+            # computing the Simpson's index of diversity
+            class_diversity = sapply(classify_results, function(x) get.dispersion(x))
+            
+            # reshaping the results
+            classify_results = t(sapply(classify_results, rbind))
+            rownames(classify_results) = mfw
+            colnames(classify_results) = predicted_classes
+            
+            
+            # collecting the results
+            accuracy_all = cbind(accuracy_all, accuracy)
+            diversity_all = cbind(diversity_all, class_diversity)
+            confusion_matrices_all[[counter.alt]] = classify_results
+        
         }
-        #
-        names(confusion.matrix) = paste("mfw_", mfw, sep = "")
-        # collecting the results
-        compact.results = rbind(compact.results, unlist(classify) )
-        full.results[[counter.alt]] = confusion.matrix
+    
+
+    
+    # naming the accuracy results' rows and columns
+    rownames(accuracy_all) = paste(mfw, "mfw", sep = "_")
+    colnames(accuracy_all) = sample.size.coverage
+    
+    # naming the class diversity results
+    rownames(diversity_all) = paste(mfw, "mfw", sep = "_")
+    colnames(diversity_all) = sample.size.coverage
+    
+    # adding the current scores to the joint object
+    joint.accuracy.scores[[iteration.counter]] = accuracy_all
+    joint.diversity.scores[[iteration.counter]] = diversity_all
+    
+#    names(full.results) = paste("sample_", sample.size.coverage, sep = "")
+#    joint.confusion.matrices[[iteration.counter]] = full.results
     
     }
     
-    
-    colnames(compact.results) = mfw
-    rownames(compact.results) = sample.size.coverage
-    joint.attribution.scores[[iteration.counter]] = compact.results
-    
-    names(full.results) = paste("sample_", sample.size.coverage, sep = "")
-    joint.confusion.matrices[[iteration.counter]] = full.results
-    
-    }
-    
+    message("")
     
     # attaching some names to the variable containing the results
-    names(joint.attribution.scores) = test.texts
-    names(joint.confusion.matrices) = test.texts
+    names(joint.accuracy.scores) = test.texts
+    names(joint.diversity.scores) = test.texts
 
+    return(joint.diversity.scores)
     
-    
-    
-    return(compact.results)
-    
-
 }
