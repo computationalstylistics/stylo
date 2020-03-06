e4cafa87f51ca813356f8a693a4491fca5a93a9d
Aaron Dunmore <aedunmore@gmail.com>
2020-03-06 11:36:20 -0500

    Major changes to txt.to.words.ext(): - no longer based on txt.to.words() (left that function unchanged so that dependencies are not affected) - rewrote control flow to be easier to read, more modular, and more DRY (don't repeat yourself): - splitting.rule logic in its own function - all splitting happens in one spot (formerly there were 4 different cases where splitting could happen)
    - replaced base::strsplit with stringr::str_split. On my machine/corpus, this runs 3-4x faster.

--- R/txt.to.words.ext.R
+++ R/txt.to.words.ext.R
@@ -2,135 +2,152 @@
 # #################################################
 # The function for splitting a given input text into
 # single words (chains of characters delimited with
-# spaces or punctuation marks). It is build on top
-# of the function txt.to.words() and it is designed
-# to manage some language-dependent text features 
-# during the process of splitting.
+# spaces or punctuation marks). Manages some 
+# language-dependent text features during the process
+# of splitting.
 # Required argument: string of characters (words)
 # Optional argument: corpus.lang
 # Supported languages: "English", "English.contr",
 # "English.all", "Latin.corr", ...
 # #################################################
 
-txt.to.words.ext = function(input.text, 
-         corpus.lang = "English", 
-         splitting.rule = NULL,
-         preserve.case = FALSE) {
-
-                 
-     # since the function can be applied to lists and vectors,
-     # we need to define an internal function that will be applied afterwards
-     wrapper = function(input.text = input.text, 
-                        corpus.lang = corpus.lang, 
-                        splitting.rule = splitting.rule,
-                        preserve.case = preserve.case) {
-                 
-                 
-  # if a custom splitting rule was detected...
-  if(length(splitting.rule) > 0) {
-      # sanity check
-      if(length(splitting.rule) == 1) {
-        # just in case, convert to characters
-        splitting.rule = as.character(splitting.rule)
-        # splitting into units specified by custom regular expression
-        tokenized.text = txt.to.words(input.text, splitting.rule, preserve.case)
-      } else {
-        stop("Wrong splitting regexp")
-      }
+txt.to.words.ext <- function(input.text,
+                             corpus.lang = "English",
+                             splitting.rule = NULL,
+                             preserve.case = FALSE) {
   
-  # if no custom splitting rule was detected...
-  } else {
-    # Loading the file; optionally, fiddling with apostrophes and contractions:    #
-    # This is the standard procedure of splitting input texts
-    if(tolower(corpus.lang) == "cjk") {
-      tokenized.text = txt.to.words(input.text,  
-                                    splitting.rule = paste("[^A-Za-z",
-                                        # Japanese (Hiragana)
-                                        "\U3040-\U309F",
-                                        # Japanese (Katagana):
-                                        "\U30A0-\U30FF",
-                                        # Japanese repetition symbols:
-                                        "\U3005\U3031-\U3035",
-                                        # CJK Unified Ideographs: 
-                                        "\U4E00-\U9FFF",
-                                        # CJK Unified Ideographs Extension A: 
-                                        "\U3400-\U4DBF",
-                                        # Hangul (Korean script):
-                                        "\UAC00-\UD7AF",
-                                        "]+", sep=""),
-                                    preserve.case=TRUE)
+  # if no valid splitting rule, set splitting rule
+  # based on corpus.lang
+  if (length(splitting.rule) > 1) {
+    stop("Wrong splitting regexp")
+  } else if (length(splitting.rule) == 0) {
+    splitting.rule = get.splitting.rule(corpus.lang)
+  }
+  
+  # don't covert CJK corpora to lower case
+  if (corpus.lang == 'CJK') {
+    preserve.case <- TRUE
+  }
+  
+  # process text: split, convert to lower case, a few
+  # other lang-specific processing steps
+  tokenized.text <- txt.processing(input.text,
+                                   splitting.rule,
+                                   preserve.case,
+                                   corpus.lang)
+  
+  return(tokenized.text)
+}
+
+txt.processing <-
+  function(input.text, splitting.rule, preserve.case, corpus.lang) {
+    
+    # converting characters to lowercase if necessary
+    if (!(preserve.case)) {
+      input.text = tryCatch(
+        tolower(input.text),
+        error = function(e)
+          NULL
+      )
+      if (is.null(input.text) == TRUE) {
+        input.text = "empty"
+        cat("turning into lowercase failed!\n")
       }
-    if(tolower(corpus.lang) != "english.contr" & 
-       tolower(corpus.lang) != "english.all" & tolower(corpus.lang) != "cjk" ) {
-    tokenized.text = txt.to.words(input.text, preserve.case=preserve.case)
     }
-    # if the Latin option with adjusting the v/u letters is on,
-    # this smashes the distinction and converts both types to the letter u
-    if(tolower(corpus.lang) == "latin.corr") {
-      tokenized.text = gsub("v", "u", tokenized.text)
-      tokenized.text = gsub("&", "et", tokenized.text)
-      tokenized.text = gsub("j", "i", tokenized.text)
-    }
-    # this code is used for English corpora only
-    if(tolower(corpus.lang) == "english.contr" | tolower(corpus.lang) == "english.all") {
-      # turning into lowercase, if applicable
-        if(preserve.case == FALSE) {
-          input.text = tolower(input.text)
-        }
-      # replacing non-ASCII apostrophes with simple ' (standard ASCII char)
-### TEMPORARILY SWITCHED OFF, due to some CRAN restrictions
-### tokenized.text = gsub(iconv("\u2019",from="UTF-8"),"'",input.text)
-tokenized.text = input.text
-      # getting rid of contractions ('t, 's, 've, 'd, 'll, 'em, 'im, 're) by 
-      # replacing their apostrophes with ^ (other apostrophes will not 
-      # be replaced); of course, if your corpus is Cockney, you should edit 
-      # the "([tsdm]|ll|ve|em|im)" statement accordingly.
-      tokenized.text = gsub("([[:alpha:]])'([tsdm]|ll|ve|em|im|re)\\b","\\1^\\2",
-                            tokenized.text) 
+    
+    # Special processing for English corpora:
+    # getting rid of contractions ('t, 's, 've, 'd, 'll, 'em, 'im, 're) by
+    # replacing their apostrophes with ^ (other apostrophes will not
+    # be replaced); of course, if your corpus is Cockney, you should edit
+    # the "([tsdm]|ll|ve|em|im)" statement accordingly.
+    if (corpus.lang == 'english.contr' |
+        corpus.lang == 'english.all') {
+      input.text = gsub("([[:alpha:]])'([tsdm]|ll|ve|em|im|re)\\b",
+                        "\\1^\\2",
+                        input.text)
       # adding spaces around dashes (to distinguish dashes and hyphens)
-      tokenized.text = gsub("[-]{2,5}"," -- ",tokenized.text)
-      # depending on which option was swithed on, either the contractions are
-      # kept, or all the peculiarities, i.e. both contractions and hyphens
-        if(tolower(corpus.lang) == "english.contr") {
-          tokenized.text=c(unlist(strsplit(tokenized.text,
-                    "[^A-Za-z\U00C0-\U00FF\U0100-\U01BF\U01C4-\U02AF^]+")))
-        }
-        if(tolower(corpus.lang) == "english.all") {
-          tokenized.text=c(unlist(strsplit(tokenized.text,
-                    "[^A-Za-z\U00C0-\U00FF\U0100-\U01BF\U01C4-\U02AF^-]+")))
-          # trying to clean the remaining dashes:
-          tokenized.text = gsub("^[-]+$","",tokenized.text)
-        }
+      input.text = gsub("[-]{2,5}", " -- ", input.text)
     }
-    # trying to avoid empty strings:
-    tokenized.text = tokenized.text[nchar(tokenized.text)>0]
-    #
+    
+    # splitting the text
+    input.text = c(unlist(stringr::str_split(input.text, splitting.rule)))
+    
+    # option for enlgish corpora:
+    # trying to clean the remaining dashes
+    if (corpus.lang == 'english.contr') {
+      input.text = gsub("^[-]+$", "", input.text)
+    }
+    input.text = input.text[nchar(input.text) > 0]
+    
+    return(input.text)
   }
-  
+
+get.splitting.rule <- function(corpus.lang) {
+
+  # maps corpus.lang to splitting.rule
+
+  if (corpus.lang == 'CJK') {
+    splitting.rule = paste(
+      "[^A-Za-z",
+      # Japanese (Hiragana)
+      "\U3040-\U309F",
+      # Japanese (Katagana):
+      "\U30A0-\U30FF",
+      # Japanese repetition symbols:
+      "\U3005\U3031-\U3035",
+      # CJK Unified Ideographs:
+      "\U4E00-\U9FFF",
+      # CJK Unified Ideographs Extension A:
+      "\U3400-\U4DBF",
+      # Hangul (Korean script):
+      "\UAC00-\UD7AF",
+      "]+",
+      sep = ""
+    )
+  } else if (corpus.lang == 'english.contr') {
+    splitting.rule = "[^A-Za-z\U00C0-\U00FF\U0100-\U01BF\U01C4-\U02AF^]+"
+  } else if (corpus.lang == 'english.all') {
+    splitting.rule = "[^A-Za-z\U00C0-\U00FF\U0100-\U01BF\U01C4-\U02AF^-]+"
+  } else {
+    splitting.rule = paste(
+      "[^A-Za-z",
+      # Latin supplement (Western):
+      "\U00C0-\U00FF",
+      # Latin supplement (Eastern):
+      "\U0100-\U01BF",
+      # Latin extended (phonetic):
+      "\U01C4-\U02AF",
+      # modern Greek:
+      "\U0386\U0388-\U03FF",
+      # Cyrillic:
+      "\U0400-\U0481\U048A-\U0527",
+      # Hebrew:
+      "\U05D0-\U05EA\U05F0-\U05F4",
+      # Arabic/Farsi:
+      "\U0620-\U065F\U066E-\U06D3\U06D5\U06DC",
+      # extended Latin:
+      "\U1E00-\U1EFF",
+      # ancient Greek:
+      "\U1F00-\U1FBC\U1FC2-\U1FCC\U1FD0-\U1FDB\U1FE0-\U1FEC\U1FF2-\U1FFC",
+      # Coptic:
+      "\U03E2-\U03EF\U2C80-\U2CF3",
+      # Georgian:
+      "\U10A0-\U10FF",
+      # Japanese (Hiragana)
+      "\U3040-\U309F",
+      # Japanese (Katagana):
+      "\U30A0-\U30FF",
+      # Japanese repetition symbols:
+      "\U3005\U3031-\U3035",
+      # CJK Unified Ideographs:
+      "\U4E00-\U9FFF",
+      # CJK Unified Ideographs Extension A:
+      "\U3400-\U4DBF",
+      # Hangul (Korean script):
+      "\UAC00-\UD7AF",
+      "]+",
+      sep = ""
+    )
   }
-  
-
-        # the proper procedure applies, depending on what kind of data 
-        # is analyzed
-        
-        # test if the dataset has a form of a single string (a vector)
-        if(is.list(input.text) == FALSE) {
-                # apply an appropriate replacement function
-                tokenized.text = wrapper(input.text = input.text, 
-                        corpus.lang = corpus.lang, 
-                        splitting.rule = splitting.rule,
-                        preserve.case = preserve.case)
-                # if the dataset has already a form of list
-        } else {
-                # applying an appropriate function to a corpus:
-                tokenized.text = lapply(input.text, wrapper, 
-                        corpus.lang = corpus.lang, 
-                        splitting.rule = splitting.rule,
-                        preserve.case = preserve.case)
-                class(tokenized.text) = "stylo.corpus"
-        }
-        
-
-  
-return(tokenized.text)
+  return(splitting.rule)
 }
