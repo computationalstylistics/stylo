d118a7bb18caffd1ace2ae8d6fa8b77d28351ac7
Maciej Eder <maciejeder@gmail.com>
2020-08-12 23:13:19 +0200

    SVM output fixed

--- R/perform.svm.R
+++ R/perform.svm.R
@@ -59,47 +59,37 @@ perform.svm = function(training.set,
   training.set = training.set[,(check.columns != 0)]
   test.set = test.set[,(check.columns != 0)]
 
-  
-  
-  classes = c(classes.training.set, classes.test.set)
-  # converting strings to factors
-  classes = factor(classes)
-  input.data = as.data.frame(rbind(training.set,test.set))
-  input.data = cbind(classes, input.data)
-  training.classes = c(1:length(training.set[,1]))
-  
   # an error is produced when variable names contain three dots: "..."
   # just in case, then:
-  colnames(input.data) = gsub("\\.\\.\\.","^^^",colnames(input.data))
+  colnames(test.set) = gsub("\\.\\.\\.","^^^",colnames(test.set))
   colnames(training.set) = gsub("\\.\\.\\.","^^^",colnames(training.set))
-  #
+
+
+
   # training a model
-  # a. default/custom parameters (faster but less accurate)
-  if(tune.parameters == FALSE) {
-          model = svm(classes ~ ., data = input.data, subset = training.classes, 
-#                 type = "C",
-                 kernel = svm.kernel, degree = svm.degree, coef0 = svm.coef0, 
-                 cost = svm.cost)
-  # b. the parameters tuned empirically
-  } else {
-          tuning.data = cbind(classes.training.set, as.data.frame(training.set))
-          colnames(tuning.data)[1] = "classes"
-          params = tune.svm(classes ~ ., data = tuning.data,
-                            gamma = 2^(-1:1), cost = 2^(2:4),
+  
+  if(tune.parameters == FALSE) { # a. default parameters (faster but less accurate)
+  
+          model = svm(training.set, factor(classes.training.set), kernel = svm.kernel, 
+                      degree = svm.degree, coef0 = svm.coef0, cost = svm.cost)
+   
+  } else {    # b. the parameters tuned empirically
+
+          params = tune.svm(training.set, factor(classes.training.set),
+                            gamma = 2^(-1:1), cost = 2^(1:5),
                             tunecontrol = tune.control(sampling = "boot"))
-          #
-          model = svm(classes ~ ., data = input.data, subset = training.classes, 
-#          type = "C",
-                 kernel = svm.kernel, degree = svm.degree, coef0 = svm.coef0, 
-                 cost=params$best.parameter[[2]], 
-                 gamma=params$best.parameter[[1]])
+          model = svm(training.set, factor(classes.training.set), kernel = svm.kernel, 
+                      degree = svm.degree, coef0 = svm.coef0, 
+                      cost = params$best.parameters$cost, 
+                      gamma = params$best.parameters$gamma )
+                      
   }
 
   
   #
   # testing the model on "new" data (i.e. the test.set)
-  classification.results = predict(model, input.data[,-1], decision.values=T)
-  
+  classification.results = predict(model, test.set, decision.values = TRUE)
+
   # retrieving decision values: a composite matrix
   d.values = attr(classification.results, "decision.values")
   
@@ -112,9 +102,6 @@ perform.svm = function(training.set,
   
   # combining both matrices
   xx = cbind(d.values,d.values.rev)
-
-  # getting rid of the trainig samples
-  xx = xx[-c(1:length(training.set[,1])),]
   
   # producing a table of "distances" (actually: decision values)
   selected.dist = c()
@@ -138,14 +125,12 @@ perform.svm = function(training.set,
 
 
   # starting final variables
-  classification.results = c()
   classification.scores = c()
   classification.rankings = c()
   
   for(h in 1:length(selected.dist[,1])) {
           ranked.c = order(selected.dist[h,],decreasing=TRUE)[1:no.of.candidates]
           current.sample = colnames(selected.dist)[ranked.c[1]]
-          classification.results = c(classification.results, current.sample)
           #
           current.ranking = colnames(selected.dist)[ranked.c]
           current.scores = selected.dist[h,ranked.c]
